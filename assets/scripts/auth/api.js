3160e4a8c2a2fa47c5f00b1580fe80662e9d7e4d
samuel-hawkins <samueledwardhawkins@gmail.com>
2020-08-27 10:19:26 -0400

    Added in curl scripts for user actions
    -Added in the auth folder for curl scripts and filled in code to make
    them work
    -Fixed some code in api.js and events.js

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const config = require('./../config')
+const store = require('./../store')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: data
+  })
+}
+
+const passChange = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: { Authorization: 'Bearer ' + store.user.token },
+    data: data
+  })
+}
+module.exports = {
+  signUp: signUp,
+  signIn: signIn,
+  passChange: passChange
+}
